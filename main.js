ce0097e8b1411b3af5d824273db1481526105b8b
Bruno Gongora <gongorabruno12@gmail.com>
2022-09-24 18:37:12 -0500

    Primer despliegue de pagina web

---
+++ main.js
@@ -0,0 +1,17 @@
+function show() {
+    document.getElementById('active').style.display = 'block'
+}
+
+function ocultar() {
+    document.getElementById("active").style.display = 'none'
+}
+
+/* Segunda seccion de servicios */
+
+function show_second() {
+    document.getElementById('active_second').style.display = 'block'
+}
+
+function ocultar_second() {
+    document.getElementById("active_second").style.display = 'none'
+}
