1867dc59b68c14558527fb4f41e2f683f9844395
Bruno Gongora <gongorabruno12@gmail.com>
2022-09-25 21:16:16 -0500

    Se temrino la seccion de servicios y se diseño la seccion de contacto

--- main.js
+++ main.js
@@ -15,3 +15,33 @@ function show_second() {
 function ocultar_second() {
     document.getElementById("active_second").style.display = 'none'
 }
+
+/* Tercera seccion de servicios */
+
+function show_tercero() {
+    document.getElementById('tercer_active').style.display = 'block'
+}
+
+function ocultar_tercero() {
+    document.getElementById("tercer_active").style.display = 'none'
+}
+
+/* Cuarta seccion de servicios */
+
+function show_cuarto() {
+    document.getElementById('active_cuatro').style.display = 'block'
+}
+
+function ocultar_cuarto() {
+    document.getElementById("active_cuatro").style.display = 'none'
+}
+
+/* Quinta seccion de servicios */
+
+function show_quinto() {
+    document.getElementById('active_quinto').style.display = 'block'
+}
+
+function ocultar_quinto() {
+    document.getElementById("active_quinto").style.display = 'none'
+}
